b1508f6d7a3099a28a142b396f498c7b83c77902
lioncat2002 <sankhayan2002@gmail.com>
2023-08-08 16:09:09 +0530

    refactor: added slack button

--- app/oauth/slack/page.tsx
+++ app/oauth/slack/page.tsx
@@ -1,8 +1,10 @@
-import { useSearchParams } from "next/navigation";
+"use client"
+import { redirect, useSearchParams } from "next/navigation";
 
 export default function SlackAuth(){
     const searchParams=useSearchParams();
     const code=searchParams.get("code")
+    redirect(`/?code=${code}`)
     return (
         <div>bruh</div>
     )
