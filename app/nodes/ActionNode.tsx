1cd5388768e88346749716065e0029ddb0aae63d
Tweedle2Dum <utkarsh.work1405@gmail.com>
2023-08-09 21:53:54 +0530

    ui rework color change

--- app/nodes/ActionNode.tsx
+++ app/nodes/ActionNode.tsx
@@ -17,18 +17,20 @@ export function ActionNode({data}){
     }
     
     return(
-        <div className="flex flex-col border-2 text-[1.2em] bg-white  p-2  w-96 h-96  items-center gap-3 drop-shadow-xl ">
+        <div className="flex flex-col border-2 text-[1.2em] bg-white  p-2  w-96 h-96  items-start gap-3 drop-shadow-xl ">
         <Handle type="target" position={Position.Top}/>
         <div className=" font-semibold border-2 border-violet-500 p-4 min-w-full text-center">Action</div>
         <div className="flex flex-col gap-2 items-center">
 
-            <div className="text-[1.2rem] flex gap-4 items-center">
-                App:
-                <select  className="max-w-[200px] bg-white border border-gray-300 text-gray-700 py-2 px-4 focus:outline-none focus:border-blue-500" name="" id="" defaultValue="none" onChange={onSelect}>
-                <option value="none">None</option>
-                <option value="slack">Slack</option>
-               </select>
-            </div>
+            <div className="text-[1.2rem] flex flex-col gap-4  ml-5">
+                <div className="flex gap-4 items-center">
+                    App:
+                    <select  className="max-w-[200px] bg-white border border-gray-300 text-gray-700 py-2 px-4 focus:outline-none focus:border-blue-500" name="" id="" defaultValue="none" onChange={onSelect}>
+                    <option value="none">None</option>
+                    <option value="slack">Slack</option>
+                                   </select>
+                </div>
+            
               
             {(()=>{
                 switch(selected){
@@ -36,6 +38,7 @@ export function ActionNode({data}){
                         return <SlackComponent data={data} />
                 }
             })()}
+            </div>
         </div>
         </div>
         
